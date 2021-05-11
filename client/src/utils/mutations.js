5e39c5c987a10d8bd9255e05841f1edc9cac344d
Johann Taylor <j.taylor1343@gmail.com>
2021-05-10 22:51:11 -0400

    Added queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,54 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, password: String!) {
+    adduser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: Bookinput!) {
+        saveBook(input: $input) {
+            username
+            bookCount
+            savedBooks {
+                title
+                authors
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            username
+            bookCount
+            savedBooks {
+                title
+                authors
+                description
+                image
+                link
+            }
+        }
+    }
+`;
